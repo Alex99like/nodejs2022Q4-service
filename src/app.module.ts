ed2fdb088b558f55f877a10707ac60ee9e2f555f
Alexander <aleksievisa@gmail.com>
2023-02-04 05:34:02 +0300

    feat: port keep env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,13 @@ import {ArtistModule} from "./artist/artist.module";
 import {AlbumModule} from "./album/album.module";
 import {TrackModule} from "./track/track.module";
 import {FavsModule} from "./favs/favs.module";
+import {ConfigModule} from "@nestjs/config";
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     DbModule,
     UserModule,
     ArtistModule,
