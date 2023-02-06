19e71fc9c255c604bc270511134150f097ea3722
Alexander <aleksievisa@gmail.com>
2023-02-06 12:40:36 +0300

    fix: fix scope

--- src/db/db.service.ts
+++ src/db/db.service.ts
@@ -3,13 +3,15 @@ import { IUser } from '../user/types/user.interface';
 import { IArtist } from '../artist/types/artist.interface';
 import { IAlbum } from '../album/types/album.interface';
 import { Favorites } from '../favs/types/favs.interface';
+import {ITrack} from "../track/types/track.interface";
+
 
 @Injectable()
 export class DbService {
   private readonly users: IUser[] = [];
   private readonly artists: IArtist[] = [];
-  private readonly tracks = [];
-  private readonly albums = [];
+  private readonly tracks: ITrack[] = [];
+  private readonly albums: IAlbum[] = [];
   private readonly favorites: Favorites = {
     artists: [],
     albums: [],
--- src/favs/types/favs.interface.ts
+++ src/favs/types/favs.interface.ts
@@ -1,5 +1,6 @@
 import { IArtist } from '../../artist/types/artist.interface';
 import { IAlbum } from '../../album/types/album.interface';
+import {ITrack} from "../../track/types/track.interface";
 
 export interface Favorites {
   artists: IArtist[];
