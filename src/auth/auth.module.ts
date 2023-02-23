6264ab0cdc04485b19f5a82c93a795c40335fbe8
Alexander <aleksievisa@gmail.com>
2023-02-24 01:01:50 +0300

    feat: add jwt strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { getJwtConfig } from 'src/config/jwt.config';
+import { UserEntity } from 'src/user/entities/user.entity';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
+  imports: [
+    TypeOrmModule.forFeature([UserEntity]),
+    ConfigModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getJwtConfig,
+    }),
+  ],
 })
 export class AuthModule {}
